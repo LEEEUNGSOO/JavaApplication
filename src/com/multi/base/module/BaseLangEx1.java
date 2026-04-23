27bd64cc5aaf7e17c64b8d5429be19df190a7e58
les <sistsay@gmail.com>
2026-04-23 14:26:45 +0900

    oo

--- src/com/multi/base/module/BaseLangEx1.java
+++ src/com/multi/base/module/BaseLangEx1.java
@@ -12,14 +12,33 @@ public class BaseLangEx1 {
         System.out.println(obj.toString());
     }
     public void objectMethod2(Object data){
+        if(data instanceof String )
         System.out.println(((String)(data)).charAt(0));
+        if(data instanceof Integer )
         System.out.println(((Integer)(data)).intValue());
+        if(data instanceof Double )
+            System.out.println(((Double)(data)).doubleValue());
+
+    }
+    public void objectMethod3(){
+        String str1=new String("abc");
+        String str2=new String("abc");
+        System.out.println(str1==str2);//false
+        System.out.println(str1.hashCode()==str2.hashCode());
+        String str3="java";//암시적
+        String str4="spring";
+        System.out.println(str3.startsWith("j"));//true
+        System.out.println(str4.startsWith("j"));//false
+        System.out.println( str3.equals(str3));//true
+        System.out.println( str3.equals("JAVA"));//false
+
     }
 
     public static void main(String[] args) {
         BaseLangEx1 base=new BaseLangEx1();
-        base.objectMethod2("Hello");
-        base.objectMethod2(10);
+//        base.objectMethod2("Hello");
+//        base.objectMethod2(10);
+          base.objectMethod3();
 
     }
 }
---
+++ src/com/multi/base/module/StringEx.java
@@ -0,0 +1,16 @@
+package com.multi.base.module;
+
+public class StringEx {
+    private char []value;
+    public StringEx(){
+
+    }
+    public StringEx(char []value){
+        this.value=value;
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}

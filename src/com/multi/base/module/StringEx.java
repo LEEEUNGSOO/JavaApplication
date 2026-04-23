4dd9023add271cfbf5e056371f64baed7e17d86b
les <sistsay@gmail.com>
2026-04-23 14:36:54 +0900

    oo

--- src/com/multi/base/module/StringEx.java
+++ src/com/multi/base/module/StringEx.java
@@ -8,8 +8,23 @@ public class StringEx {
     public StringEx(char []value){
         this.value=value;
     }
+    public boolean equals(Object obj){
+        if(this ==obj)return true;
+        if(obj instanceof String){
+            char []temp=((String)(obj)).toCharArray();
+            if(temp.length!=value.length)return false;
+            for (int i = 0; i <this.value.length ; i++) {
+                if(value[i]!=temp[i])return false;
+            }//for
+            return  true;
+        }
+        return false;
+    }
 
     public static void main(String[] args) {
+        StringEx str1=new StringEx("abc".toCharArray());//  value[a][b][c]
+        StringEx str2=new StringEx("def".toCharArray());//  value[d][e][f]
+        System.out.println( str1.equals("ab"));
 
     }
 

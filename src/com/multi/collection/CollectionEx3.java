ede9ea2f05686a25c8f15cc3f91526fc5246d7a5
les <sistsay@gmail.com>
2026-04-29 13:39:43 +0900

    test

--- src/com/multi/collection/CollectionEx3.java
+++ src/com/multi/collection/CollectionEx3.java
@@ -2,6 +2,7 @@ package com.multi.collection;
 
 import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.Iterator;
 
 //List,Set,Map
 public class CollectionEx3 {
@@ -11,17 +12,30 @@ public class CollectionEx3 {
         list1.add("딸기");
         list1.add("수박");
         list1.add("멜론");
-        System.out.println(list1.toString());
 
+//        for(int i=0;i<list1.size();i++){
+//            System.out.println(list1.get(i));
+//        }
+//          for(String s :list1){
+//              System.out.println(s);
+//          }
+
+        //System.out.println(list1.toString());
+        //System.out.println( list1.get(1).startsWith("딸기"));
+        Iterator<String>iter= list1.iterator();//반복자 객체
+        while(iter.hasNext()){
+            System.out.println(iter.next());
+        }
         HashSet<Integer>set1=new HashSet<>();
         set1.add(10);
         set1.add(20);
         set1.add(20);
+        Iterator<Integer> it=set1.iterator();
+        for(;it.hasNext();){
+            System.out.println(it.next());
+        }
 
-        System.out.println(set1);
-
-
-
+        //System.out.println(set1);
 
     }
 }
